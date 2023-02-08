a8337d0399ce040795ddec6f220a96e3b0777f7f
Nicolai Holmgaard Okkels <nho@commentor.dk>
2023-02-08 12:51:06 +0100

    C# and Python file parser

---
+++ 02_data_formats/exercise_01/python/file_parser/parser.py
@@ -0,0 +1,29 @@
+import json, yaml, csv, xml.etree.ElementTree as ET
+
+class Parser:
+    def __init__(self):
+        self.supported_file_types = ["txt", "xml", "yaml", "yml", "json", "csv"]
+    
+    def parse_file(self, file_path):
+        file_type = file_path.split(".")[-1]
+        if file_type not in self.supported_file_types:
+            print(f"file type not supported: {file_type}")
+            return
+
+        with open(file_path, "r") as file:
+            file_content = file.read()
+            if file_type == "txt":
+                print(file_content)
+            elif file_type == "xml":
+                root = ET.fromstring(file_content)
+                print(ET.tostring(root, encoding="unicode"))
+            elif file_type == "yaml":
+                data = yaml.safe_load(file_content)
+                print(data)
+            elif file_type == "json":
+                data = json.loads(file_content)
+                print(json.dumps(data))
+            elif file_type == "csv":
+                reader = csv.reader(file_content.splitlines())
+                for row in reader:
+                    print(row)
---
+++ 02_data_formats/exercise_01/python/main.py
@@ -0,0 +1,9 @@
+import file_parser
+
+f = file_parser.Parser()
+
+f.file_path("files/me.txt")
+f.file_path("files/me.json")
+f.file_path("files/me.xml")
+f.file_path("files/me.csv")
+# f.file_path("me.yaml")
