b79767a88c79ad19bf244ef72e383afb92a904b6
nicolaiokkels <nico902t@stud.kea.dk>
2023-02-12 15:31:27 +0100

    removed comments

--- 02_data_formats/exercise_01/csharp/Program.cs
+++ 02_data_formats/exercise_01/csharp/Program.cs
@@ -9,11 +9,11 @@ namespace FileParser
         {
             Parser parser = new Parser();
 
-            parser.ParseFile("../files/me.txt"); // works
-            parser.ParseFile("../files/me.json"); // works
+            parser.ParseFile("../files/me.txt");
+            parser.ParseFile("../files/me.json");
             //parser.ParseFile("../files/me.yaml"); //TODO: fix
-            parser.ParseFile("../files/me.xml"); // works
-            parser.ParseFile("../files/me.csv"); // fix
+            parser.ParseFile("../files/me.xml"); 
+            //parser.ParseFile("../files/me.csv"); //TODO: fix
         }
     }
 }
