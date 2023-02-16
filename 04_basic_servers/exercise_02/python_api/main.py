5f7fec367f8e0a6ffa71619aabdc2f39e99b0f9c
nicolaiokkels <nico902t@stud.kea.dk>
2023-02-16 11:49:56 +0100

    import file parser from exercise 01

--- 04_basic_servers/exercise_02/python_api/main.py
+++ 04_basic_servers/exercise_02/python_api/main.py
@@ -1,29 +1,36 @@
 from fastapi import FastAPI
+import file_parser
 
 app = FastAPI()
+parser = file_parser.Parser()
 
 @app.get("/")
 def read_root():
     return {"Test": "Server"}
 
-@app.get("")
+@app.get("/xml")
 def read_xml():
+    parser.parse_file("./02_data_formats/exercise_01/files/me.xml")
     return {}
 
-@app.get("")
+@app.get("/csv")
 def read_csv():
+    parser.parse_file("./02_data_formats/exercise_01/files/me.csv")
     return {}
 
-@app.get("")
+@app.get("/yaml")
 def read_yaml():
+    parser.parse_file("./02_data_formats/exercise_01/files/me.yaml")
     return {}
 
-@app.get("")
+@app.get("/txt")
 def read_txt():
+    parser.parse_file("./02_data_formats/exercise_01/files/me.txt")
     return {}
 
-@app.get("")
+@app.get("/json")
 def read_json():
+    parser.parse_file("./02_data_formats/exercise_01/files/me.json")
     return {}
 
 #File types: XML, CSV, YAML, TXT, JSON.
